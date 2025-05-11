53a29ec5376b8303e5cb36f32ea28d597003388e
Mahan Maleki <mahanmaleki950@gmail.com>
2025-05-11 19:55:46 +0330

    write basic of App.jsx file codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <main>
+        <div className="container">
+          <div className="row">
+
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
 

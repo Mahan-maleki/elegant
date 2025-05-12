479dc7291ca85d6c84255706ec845cbb65bb0f21
Mahan Maleki <mahanmaleki950@gmail.com>
2025-05-12 13:56:52 +0330

    write discount section code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { ArrowLeft, TicketPercent } from 'lucide-react'
+import { NavLink } from 'react-router'
+import css from "./Header.module.css"
 
 function Header() {
   return (
-    <div>Header</div>
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className={"col-12 " + css.discount}>
+            <div className="row m-0 d-flex justify-content-center">
+              <div className="col-1 d-flex justify-content-end">
+                <TicketPercent color='#000' size={24} className='d-none d-lg-block' />
+              </div>
+              <div className="col-12 col-lg-3 d-flex d-lg-block justify-content-center">
+                <span>30% تخفیف بر روی تمامی محصولات | زمان محدود !</span>
+              </div>
+              <div className="col-1 d-none d-lg-block">
+                <NavLink to="/shop" className={"text-decoration-none " + css.discountLink}>برو به فروشگاه <ArrowLeft color='#377DFF' size={15} /></NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 

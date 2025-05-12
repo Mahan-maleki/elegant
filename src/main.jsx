bb5a2129b8b99965ace364a01edbdb6f1ce7cbf0
Mahan Maleki <mahanmaleki950@gmail.com>
2025-05-12 18:35:41 +0330

    remove all page imports and tags in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import Shop from "./components/Shop"
-import Product from "./components/Product"
 import ContactUs from "./components/ContactUs"
 import SignIn from "./components/SignIn"
 import SignUp from "./components/SignUp"
@@ -16,7 +15,6 @@ ReactDOM.createRoot(root).render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/shop" element={<Shop />} />
-      <Route path="/product" element={<Product />} />
       <Route path="/contact" element={<ContactUs />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />

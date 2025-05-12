95cd2a84266efd4a728284d8bca1af25ab9f1f2e
Mahan Maleki <mahanmaleki950@gmail.com>
2025-05-12 18:53:57 +0330

    make a desktop header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ArrowLeft, TicketPercent } from 'lucide-react'
+import { ArrowLeft, CircleUser, Search, ShoppingBag, TicketPercent } from 'lucide-react'
 import { NavLink } from 'react-router'
 import css from "./Header.module.css"
 
@@ -18,6 +18,38 @@ function Header() {
               </div>
               <div className="col-1 d-none d-lg-block">
                 <NavLink to="/shop" className={"text-decoration-none " + css.discountLink}>برو به فروشگاه <ArrowLeft color='#377DFF' size={15} /></NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className={"row d-flex align-items-center " + css.header}>
+          <div className="col-2 d-flex align-items-center">
+            <NavLink to="/">
+              <img src="/Logo.svg" alt="logo" className={css.logo} />
+            </NavLink>
+          </div>
+          <div className="col-8 d-flex align-items-center justify-content-center">
+            <ul className='mb-0 d-flex'>
+              <li className={css.menuList}><NavLink to="/" className="text-decoration-none text-dark fs-5 px-4">خانه</NavLink></li>
+              <li className={css.menuList}><NavLink to="/shop" className="text-decoration-none text-dark fs-5 px-4">فروشگاه</NavLink></li>
+              <li className={css.menuList}><NavLink to="/contact" className="text-decoration-none text-dark fs-5 px-4">تماس باما</NavLink></li>
+            </ul>
+          </div>
+          <div className="col-2 d-flex align-items-center">
+            <div className="row">
+              <div className="col-3">
+                  <Search color='#000' size={24} strokeWidth={1.5} />
+              </div>
+              <div className="col-3">
+                <CircleUser color='#000' size={24} strokeWidth={1.5} />
+              </div>
+              <div className="col-5 d-flex align-items-center">
+                <ShoppingBag color='#000' size={24} strokeWidth={1.5} />
+                <div className={css.cartBadge + " me-2 rounded-circle d-flex justify-content-center align-items-center"}>
+                  <span className='text-white'>0</span>
+                </div>
               </div>
             </div>
           </div>

66ccec8dba6858cbf6c7635e7ec4c371fe54b623
Mahan Maleki <mahanmaleki950@gmail.com>
2025-05-13 19:31:25 +0330

    make offcanvas for mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import { ArrowLeft, CircleUser, Menu, Search, ShoppingBag, TicketPercent } from 'lucide-react'
+import React, { useState } from 'react'
+import { ArrowLeft, CircleUser, Menu, Search, ShoppingBag, TicketPercent, X } from 'lucide-react'
 import { NavLink } from 'react-router'
 import css from "./Header.module.css"
 
 function Header() {
+
+  const [menuStatus, setMenuStatus] = useState(false);
+
   return (
     <>
       <div className="container-fluid">
@@ -57,8 +60,9 @@ function Header() {
       </div>
       <div className="container d-block d-lg-none">
         <div className={"row " + css.mobileHeader}>
+          {menuStatus == true && <div className={css.mobileMenu + ` d-${menuStatus == true ? "block" : "none"}`}><div className={css.offcanvas + " animate__animated animate__fadeInOut"}><div className="col-12"><div className="row"><div className="col-12 mb-2"><div className="row d-flex align-items-center justify-content-between"><div className="col-3"><img src="/Logo.svg" alt="website logo" width={70} /></div><div className="col-1"><X color='#6C7275' size={24} onClick={() => setMenuStatus(false)} /></div></div></div><div className="col-12 mt-4"><input type="text" className='form-control border-2' placeholder='جست و جو' /></div><div className="col-12 mt-4"><ul><li className={css.mobileMenuList}><NavLink to="/" className="text-decoration-none text-dark elegant-demibold" >خانه</NavLink></li><hr /><li className={css.mobileMenuList}><NavLink to="/shop" className="text-decoration-none text-dark elegant-demibold" >فروشگاه</NavLink></li><hr /><li className={css.mobileMenuList}><NavLink to="/contact" className="text-decoration-none text-dark elegant-demibold" >تماس باما</NavLink></li></ul></div><div className="col-12 d-flex justify-content-center"><button className='position-absolute bottom-0 mb-3 px-5 btn btn-dark btn-lg shadow-lg'><NavLink className="text-decoration-none text-white fs-6" to="/signin">ورود | ثبت نام</NavLink></button></div></div></div></div></div>}
           <div className="col-1">
-            <Menu color='#000' size={24} />
+            <Menu color='#000' size={24} onClick={() => setMenuStatus(true)} />
           </div>
           <div className="col-8">
             <img src="/Logo.svg" alt="website logo" className='me-2' width={70} />

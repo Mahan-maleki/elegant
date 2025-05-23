373707076708a0f23e681cdf33ce3bbc1975ecce
Mahan Maleki <mahanmaleki950@gmail.com>
2025-05-23 14:16:56 +0330

    create and make a Counter structure component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { Plus, Minus } from "lucide-react"
 
-function Counter() {
+function Counter(props) {
+
+  const [num, setNum] = useState(1);
+
   return (
-    <div>Counter</div>
+    <>
+      <div className="col-12 p-2 d-flex justify-content-between align-items-center border border-black border-1 rounded rounded-1">
+          <Plus color='#000' size={16} strokeWidth={1.25} onClick={() => setNum(num <= 9 ? num + 1 : num)} />
+          <span>{num}</span>
+          <Minus color='#000' size={16} strokeWidth={1.25} onClick={() => setNum(num >= 1 ? num - 1 : num)} />
+      </div>
+    </>
   )
 }
 
